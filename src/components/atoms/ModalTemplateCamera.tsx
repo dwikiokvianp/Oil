508a6d8bde03fecdafbebfceb59c2fc72d30067a
dwikiokvianpradana <dwikiokvianp1999@gmailcom>
2023-06-19 11:34:21 +0700

    feat - add camera modal

---
+++ src/components/atoms/ModalTemplateCamera.tsx
@@ -0,0 +1,57 @@
+import { Fragment, useRef } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
+export default function ModalTemplate({
+  open,
+  setOpen,
+  innerComponent,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  innerComponent: JSX.Element;
+}) {
+  const cancelButtonRef = useRef(null);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10 "
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="bg-black relative transform overflow-hidden rounded-xl shadow-xl bg-white px-4 py-8 pb-8 text-left shadow-xl transition-all sm:my-2 sm:w-full sm:max-w-lg sm:p-6">
+                <div className="py-4">
+                  <div>{innerComponent}</div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
--- src/pages/Officer/RootOfficer.tsx
+++ src/pages/Officer/RootOfficer.tsx
@@ -5,7 +5,18 @@ import { OfficerNavigation } from "../../components/organisms/OfficerNavigation.
 import {
   getLocalStorage,
   LocalStorageKeys,
+  removeLocalStorage,
 } from "../../utils/local.storage.utils.ts";
+import { classNames } from "../../utils/class.mapper.utils.ts";
+
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
+import { useNavigate } from "react-router-dom";
+import {
+  addNotification,
+  addNotificationWithConfirm,
+} from "../../utils/notification.utils.ts";
 
 const officerNavigation = [
   {
@@ -45,13 +56,77 @@ export function RootOfficer() {
     }
   }, [open]);
 
+  const navigate = useNavigate();
+
   return (
     <main className="m-8">
       <header className="mb-10 flex justify-between">
         <h1 className="font-semibold text-xl">
           Hello, {getLocalStorage(LocalStorageKeys.name)?.toUpperCase()}
         </h1>
-        <img src="../../../public/dotted.svg" alt="" />
+        <section>
+          <Menu as="div" className="relative flex-none">
+            <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+              <span className="sr-only">Open options</span>
+              <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      onClick={() => {
+                        addNotificationWithConfirm(
+                          "Are you sure want to logout?"
+                        ).then((res) => {
+                          if (res.isConfirmed) {
+                            removeLocalStorage(LocalStorageKeys.name);
+                            removeLocalStorage(LocalStorageKeys.token);
+                            removeLocalStorage(LocalStorageKeys.role);
+                            removeLocalStorage(LocalStorageKeys.email);
+                            addNotification("success", "Logout success");
+                            navigate("/login");
+                          } else {
+                            addNotification("info", "Logout canceled");
+                          }
+                        });
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-50" : "",
+                        "block px-3 py-1 text-sm leading-6 text-gray-900"
+                      )}
+                    >
+                      Logout
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      onClick={() => {
+                        addNotification("info", "Setting feature Coming soon");
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-50" : "",
+                        "block px-3 py-1 text-sm leading-6 text-gray-900"
+                      )}
+                    >
+                      Setting
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </section>
       </header>
       <section>
         <div className="grid grid-cols-2 gap-4 ">
