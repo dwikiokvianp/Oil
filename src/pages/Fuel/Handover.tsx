d16c6e94e4f28edbe882f3209b8604cca5ed8770
dwikiokvianpradana <dwikiokvianp1999@gmailcom>
2023-06-22 12:48:52 +0700

    feat - add skeleton for personal information

--- src/pages/Fuel/Handover.tsx
+++ src/pages/Fuel/Handover.tsx
@@ -1,9 +1,13 @@
 import { HeaderOfficerTitle } from "../../components/molecules/HeaderOfficerTitle.tsx";
+import { MdDomainDisabled } from "react-icons/md";
 
 export function Handover() {
   return (
     <div className="m-6">
       <HeaderOfficerTitle title={"Handover List"} />
+      <div className=" w-full h-[90vh] flex justify-center items-center text-7xl">
+        <MdDomainDisabled />
+      </div>
     </div>
   );
 }
