1c3f4007bc13ad5fd60d70c4c92a9a31b04b1c74
dwikiokvianpradana <dwikiokvianp1999@gmailcom>
2023-06-26 11:22:48 +0700

    feat - add endpoint for region city and province

---
+++ src/api/region.service.api.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const regionServiceApi = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+export interface City {
+  id: number;
+  name: string;
+  province_id: number;
+}
+
+export interface GetCityResponse {
+  data: City[];
+}
+
+export interface ProvinceResponse {
+  id: number;
+  name: string;
+  cities: City[];
+}
+
+export interface GetProvinceResponse {
+  data: ProvinceResponse[];
+}
+
+export const getRegionCity = async (): Promise<GetCityResponse> => {
+  const { data } = await regionServiceApi.get("/city");
+  return data;
+};
+
+export const getRegionProvince = async (): Promise<GetProvinceResponse> => {
+  const { data } = await regionServiceApi.get("/province");
+  return data;
+};
+
+export const getRegionCityById = async (id: number) => {
+  const { data } = await regionServiceApi.get(`/city/${id}`);
+  return data;
+};
+
+export const getRegionProvinceById = async (id: number) => {
+  const { data } = await regionServiceApi.get(`/province/${id}`);
+  return data;
+};
