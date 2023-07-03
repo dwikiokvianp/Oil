46541bfc1f215971a1c34f312a8fea8eebdc9f60
dwikiokvianpradana <dwikiokvianp1999@gmailcom>
2023-07-03 15:16:00 +0700

    feat - add getwarehouse api service

--- src/api/warehouse.service.api.ts
+++ src/api/warehouse.service.api.ts
@@ -1,3 +1,16 @@
 import axios from "axios";
 
-const warehouseServiceApi = axios.create({});
+const warehouseServiceApi = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+interface WarehouseData {
+  id: number;
+  name: string;
+  location: string;
+}
+
+export const getWarehouses = async (): Promise<WarehouseData[]> => {
+  const { data } = await warehouseServiceApi.get("/warehouses");
+  return data;
+};
