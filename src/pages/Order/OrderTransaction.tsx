9762988050166835c6d3db470ebb38fbb8059242
dwikiokvianpradana <dwikiokvianp1999@gmailcom>
2023-07-07 00:39:39 +0700

    feat - add proportional city and province

--- src/pages/Order/OrderTransaction.tsx
+++ src/pages/Order/OrderTransaction.tsx
@@ -332,6 +332,32 @@ export function OrderTransaction() {
                   />
                 </div>
               </div>
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="country"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Officer
+                </label>
+                <div className="mt-2">
+                  <select
+                    id="country"
+                    name="country"
+                    autoComplete="country-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    value={selectedOfficer}
+                    onChange={(e) => {
+                      setSelectedOfficer(Number(e.target.value));
+                    }}
+                  >
+                    {Officer?.data.map((ship) => (
+                      <option key={ship.id} value={ship.id}>
+                        {ship.username}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
               {!isPickup ? (
                 <>
                   <div className="sm:col-span-3">
@@ -388,32 +414,6 @@ export function OrderTransaction() {
                   </div>
                 </>
               ) : null}
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="country"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Officer
-                </label>
-                <div className="mt-2">
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    value={selectedOfficer}
-                    onChange={(e) => {
-                      setSelectedOfficer(Number(e.target.value));
-                    }}
-                  >
-                    {Officer?.data.map((ship) => (
-                      <option key={ship.id} value={ship.id}>
-                        {ship.username}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
             </div>
           </div>
           <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
